83a501f6d46d683a6509927fd3124e09adcda19f
TommyM0ntana <tommy.montana.harwood@gmail.com>
2022-03-03 16:34:49 +0100

    add Redux Store and Provider

--- src/App.js
+++ src/App.js
@@ -8,23 +8,27 @@ import AddTicketModal from "./components/tickets/AddTicketModal"
 import EditTicketModal from "./components/tickets/EditTicketModal"
 import AddTechModal from "./components/techs/AddTechModal"
 import TechListModal from "./components/techs/TechListModal"
+import { Provider } from "react-redux"
+import store from "./store"
 
 const App = () => {
   useEffect(() => {
     M.AutoInit()
   })
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddTicketModal />
-        <EditTicketModal />
-        <AddTechModal />
-        <TechListModal />
-        <Tickets />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddTicketModal />
+          <EditTicketModal />
+          <AddTechModal />
+          <TechListModal />
+          <Tickets />
+        </div>
+      </Fragment>
+    </Provider>
   )
 }
 
