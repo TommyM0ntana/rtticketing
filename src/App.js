4488fc0ef4d52581f18f523ef671555788ddc11e
TommyM0ntana <tommy.montana.harwood@gmail.com>
2022-03-03 16:03:08 +0100

    add Btn and Modal component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import M from "materialize-css/dist/js/materialize.min.js"
 import React, { Fragment, useEffect } from "react"
 import SearchBar from "./components/layout/SearchBar"
 import Tickets from "./components/tickets/Tickets"
+import AddBtn from "./components/layout/AddBtn"
+import AddTicketModal from "./components/tickets/AddTicketModal"
 
 const App = () => {
   useEffect(() => {
@@ -12,6 +14,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddTicketModal />
         <Tickets />
       </div>
     </Fragment>
